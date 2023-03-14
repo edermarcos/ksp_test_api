fa67826bfc5eff89a42d649cd15bd35b46e1072f
Eder Marcos <eder.marcos.lara@gmail.com>
2023-03-14 00:01:09 -0600

    Some changes were made in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const logger = new Logger('Bootstrap');
   // It is important to add this line because it will allow us to use the DTOs validations
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +12,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
