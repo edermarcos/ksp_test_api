f211a1c1ba13684f8fdfc8dd8a1766d9d9a6a034
Eder Marcos <eder.marcos.lara@gmail.com>
2023-03-14 00:16:25 -0600

    Cors were added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+  app.enableCors();
   // It is important to add this line because it will allow us to use the DTOs validations
   app.useGlobalPipes(
     new ValidationPipe({
